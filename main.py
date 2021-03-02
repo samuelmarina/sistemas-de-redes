4fa1c9cc8ca25c2272641f95f6e4e88d2c1a2d9e
samuelmarina <samuellmarina@gmail.com>
2021-03-02 14:12:56 -0400

    Funciones de solicitud de input

--- main.py
+++ main.py
@@ -1,1 +1,42 @@
-from variables import cities, flights, visas, cityName
+from variables import cities, flights, visas, cityName, msgs as M
+from helpers import checkWrongAnswer, hasVisa
+
+
+def printCities():
+    """ Imprimir el arreglo de ciudades
+    """
+    for x in range(len(cities)):
+        print(str(x) + ' - ' + cities[x])
+
+
+def printWelcomeMessage():
+    """ Imprimir el mensaje de bienvenida
+    """
+    print(M['msg1'] + '\n' + M['msg2'])
+    printCities()
+
+
+def requestInput():
+    """ Pedir al usuario la información para su vuelo
+    Returns:
+        diccionario con los índices de las ciudades escogidas, si el usuario
+        cuenta o no con visa, y la ruta que desea escoger
+    """
+    L = len(cities)
+    origin = input()
+    origin = checkWrongAnswer(origin, 0, L)
+    target = input(M['msg3'] + '\n')
+    target = checkWrongAnswer(target, 0, L)
+    visa_res = input(M['msg4'] + '\n')
+    visa = hasVisa(visa_res)
+    route = input(M['msg5'] + '\n')
+    route = checkWrongAnswer(route, 1, 2)
+    return {'origin': origin, 'target': target, 'visa': visa, 'route': route}
+
+
+def start():
+    printWelcomeMessage()
+    response = requestInput()
+
+
+start()
